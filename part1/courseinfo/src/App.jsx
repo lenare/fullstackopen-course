61697d20b81c9b9b2f76b6018dc6a7fd2bb9527b
lenare <lenareichel97@gmail.com>
2025-09-07 23:23:20 +0200

    feat: exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,11 +3,17 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  return props.contents.map((element, index) => (
-    <p key={index}>
-      {element.part} {element.exercises}
-    </p>
-  ));
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  );
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
 }
 
 const Total = (props) => {
@@ -30,7 +36,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content contents={[{part: part1, exercises: exercises1}, {part: part2, exercises: exercises2}, {part: part3, exercises: exercises3}]} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
