c6f638d26dcbfac96d6443fc2d40f71ab2ea4bf8
lenare <lenareichel97@gmail.com>
2025-09-29 14:48:56 +0200

    feat: exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
 
-const FeedbackButton = ({ onClick, text }) => {
-  return <button onClick={onClick}>
-    {text}
-  </button>
+const FeedbackButton = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad
+
+  if (all > 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}<br />
+        all {all}<br />
+        average {(good - bad) / all}<br />
+        positive {(good / all) * 100}%<br />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        No feedback given
+      </div>
+    )
+  }
 }
 
 const App = () => {
@@ -12,21 +34,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  let all = good + neutral + bad
-
   return (
     <div>
       <h1>Give feedback</h1>
       <FeedbackButton onClick={() => setGood(good + 1)} text="good" />
       <FeedbackButton onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <FeedbackButton onClick={() => setBad(bad + 1)} text="bad" />
-      <h1>Statistics</h1>
-      good {good}<br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      all {all}<br />
-      average {(good - bad) / all}<br />
-      positive {(good / all) * 100}%<br />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
