e7a992a7c73f0910454e8d6ad4059dfb50037ee4
lenare <lenareichel97@gmail.com>
2025-09-30 17:01:23 +0200

    feat: exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
+import countryService from './services/countries'
+
+function App() {
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(allCountries => {
+        const filteredCountries = allCountries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+        if (filteredCountries.length > 10) {
+          setCountries([])
+        } else if (filteredCountries.length === 0) {
+          setCountries(null)
+        } else {
+          setCountries(filteredCountries)
+        }
+      }
+      )
+  }, [filter])
+
+  return (
+    <div>
+      <h2>Countries</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <Countries countries={countries} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countries/src/components/Countries.jsx
@@ -0,0 +1,34 @@
+const Countries = ({ countries }) => {
+  if (countries === null) {
+    return <div>No countries found that match the filter</div>
+  } else if (countries.length === 0) {
+    return <div>Too many matches. Please specify in the filter</div>
+  } else if (countries.length === 1) {
+    return (
+      <div>
+        <h1>{countries[0].name.common}</h1>
+        <div>
+          Capital {countries[0].capital[0]}<br />
+          Area {countries[0].area}
+        </div>
+        <h2>Languages</h2>
+        <ul>
+          {Object.entries(countries[0].languages).map(([key, language]) =>
+            <li key={key}>{language}</li>
+          )}
+        </ul>
+        <div>
+          <img src={countries[0].flags.png} alt={countries[0].flags.alt}></img>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {countries.map(country => <div key={country.name.common}>{country.name.common}</div>)}
+      </div>
+    )
+  }
+}
+
+export default Countries
