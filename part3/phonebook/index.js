f05a8d48b456579ce4478a810f502403f3e2c64c
lenare <lenareichel97@gmail.com>
2025-10-03 11:46:03 +0200

    feat: exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,16 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const person = persons.find(person => person.id === request.params.id)
+    if (person) {
+        persons.filter(person => person.id !== request.params.id)
+        response.status(204).end()
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
