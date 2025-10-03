9384b0166ae8360265d998c2ef99ce511e5a2010
lenare <lenareichel97@gmail.com>
2025-10-03 11:38:17 +0200

    feat: exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,10 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`)
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
