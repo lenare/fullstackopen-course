f79d00292185f730a35a68768f840f6c47f63c54
lenare <lenareichel97@gmail.com>
2025-10-03 13:44:31 +0200

    feat: exercise 3.11

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -9,6 +9,7 @@ morgan.token('body', function getId(req) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
